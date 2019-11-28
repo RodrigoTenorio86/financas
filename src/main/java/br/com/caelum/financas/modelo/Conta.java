cdea7d20d7882f5975362da860310c82be9d15e4
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2019-11-28 12:36:56 -0800

    criacao da classe de modelo CONTA.

---
+++ src/main/java/br/com/caelum/financas/modelo/Conta.java
@@ -0,0 +1,50 @@
+package br.com.caelum.financas.modelo;
+
+public class Conta {
+	private Integer id;
+	private String titulo;
+	private String banco;
+	private String agencia;
+	private String numero;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+}
