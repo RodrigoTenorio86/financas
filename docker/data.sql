7c5111f7496b388857b6ba69e4bc5ba34b7fd6b6
Rodrigo Tenorio de Boa Ventura <boaventura19@yahoo.com.br>
2019-11-29 07:09:43 -0800

    Primeiros passos com a JPA ,Configuração , Inicialização da JPA e Config. inicial de DOCKER.

---
+++ docker/data.sql
@@ -0,0 +1,2 @@
+create database if not exists financas;
+USE financas;
--- src/main/java/br/com/caelum/financas/modelo/Conta.java
+++ src/main/java/br/com/caelum/financas/modelo/Conta.java
@@ -1,8 +1,16 @@
 package br.com.caelum.financas.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Conta {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String titulo;
+	private String titular;
 	private String banco;
 	private String agencia;
 	private String numero;
@@ -15,12 +23,12 @@ public class Conta {
 		this.id = id;
 	}
 
-	public String getTitulo() {
-		return titulo;
+	public String getTitular() {
+		return titular;
 	}
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
+	public void setTitular(String titulo) {
+		this.titular = titulo;
 	}
 
 	public String getBanco() {
@@ -46,5 +54,32 @@ public class Conta {
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Conta other = (Conta) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 
 }
